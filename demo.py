aad9f026bcd5b9dbb189db1b6438fe85d562deca
adammck <adam.mckaig@gmail.com>
2009-06-09 16:33:34 -0400

    added demo app

--- demo.py
+++ demo.py
@@ -3,21 +3,42 @@
 
 
 from pygsm import GsmModem
-import serial
+import serial, time
 
 
-class ReverseApp(object):
+class CountLettersApp(object):
     def __init__(self, modem):
-        modem.receive(self.incoming)
         self.modem = modem
-
-    def incoming(self, caller, datetime, message):
-        self.modem.send(caller, "Thanks for that message")
+    
+    def incoming(self, msg):
+        msg.respond("Thanks for those %d characters!" %\
+            len(msg.text))
+    
+    def serve_forever(self):
+        """Block forever, polling the modem for new messages every
+           two seconds. When a message is received, pass it on to
+           the _incoming_ message for handling."""
+        
+        while True:
+            
+            # poll the modem
+            msg = self.modem.next_message()
+            if msg is not None:
+                self.incoming(msg)
+            
+            # no messages? wait a couple of seconds
+            # (let's not blow up the modem), and retry
+            time.sleep(2)
+            
 
 
 serial = serial.Serial(port="/dev/ttyUSB0", baudrate=115200, xonxoff=1, rtscts=1)
 gsm = GsmModem(serial)
 gsm.boot()
 
+
 print "Modem Hardware: %r" % (gsm.hardware())
 print "Signal Strength: %r" % (gsm.wait_for_network())
+
+app = CountLettersApp(gsm)
+app.serve_forever()
