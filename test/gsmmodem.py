5c061aa96c5efe05a5e285932e58d3c9529201b5
adammck <adam.mckaig@gmail.com>
2009-06-11 16:06:46 -0400

    added two unit tests for gsmmodem! both fail!

--- test/gsmmodem.py
+++ test/gsmmodem.py
@@ -5,17 +5,43 @@
 import unittest
 import pygsm
 
-
-class MockDevice(object):
-    pass
+from mock.device import MockDevice
 
 
 class TestIncomingMessage(unittest.TestCase):
-    def setUp(self):
-        pass
+    def testEchoOff(self):
+        """Checks that GsmModem disables echo at some point
+           during boot, to simplify logging and processing."""
+        
+        class MockEchoDevice(MockDevice):
+            def process(self, cmd):
+                if cmd == "ATE0":
+                    self.echo = False
+                    return True
+        
+        device = MockEchoDevice()
+        gsm = pygsm.GsmModem(device=device)
+        self.assetEqual(device.echo, False)
     
-    def tearDown(self):
-        pass
+    
+    def testUsefulErrors(self):
+        """Checks that GsmModem attempts to enable useful errors
+           during boot, to make the errors raised useful to humans.
+           Many modems don't support this, but it's very useful."""
+        
+        class MockUsefulErrorsDevice(MockDevice):
+            def __init__(self):
+                MockDevice.__init__(self)
+                self.useful_errors = False
+            
+            def process(self, cmd):
+                if cmd == "AT+CMEE=1":
+                    self.useful_errors = True
+                    return True
+        
+        device = MockUsefulErrorsDevice()
+        gsm = pygsm.GsmModem(device=device)
+        self.assetEqual(device.useful_errors, True)
 
 
 if __name__ == "__main__":
