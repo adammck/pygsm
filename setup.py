8dfd3eab95cfc613930b754a541f980fb6929b01
adammck <adam.mckaig@gmail.com>
2009-11-13 01:33:18 +0300

    added setup.py for 0.1 tag

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+# see LICENSE file (it's BSD)
+
+
+from setuptools import setup
+
+
+setup(
+    name="pyGSM",
+    version="0.1",
+    license="BSD",
+
+    author="Adam Mckaig",
+    author_email="adam.mckaig@gmail.com",
+
+    maintainer="RapidSMS development community",
+    maintainer_email="rapidsms@googlegroups.com",
+
+    description="Python interface to GSM modems",
+    url="http://github.com/adammck/pygsm",
+    packages=["pygsm", "pygsm.message"]
+)
